ca698b2adab559e2f2ef0e66d94a9e67de7f5725
DIEGO ESCOBAR <you@example.com>
2023-02-20 10:06:27 -0500

    avance

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
+// Elements ****************************************************
+
 const main = document.querySelector('.main')
 const cardTemplate = document.querySelector('#cardTemplate').content
 let fragment = document.createDocumentFragment()
 const searchInput = document.querySelector('#searchInput')
 const autoComplete = document.querySelector('.autocomplete')
+const filtersBtn = document.querySelector('#filtersBtn')
+const filter = document.querySelector('.filter')
+const closeFilterBtn = document.querySelector('#closeFilterBtn')
+const filterItems = document.querySelectorAll('.filter__item')
+
+// Functions ****************************************************
+
 
 const enableShower = () => {
     const cardShower = document.querySelectorAll('.card__shower')
-    let clicked = false
     for (let i = 0; i < cardShower.length; i++){
         cardShower[i].addEventListener('click', () => {
-            clicked = !clicked
-            if (clicked == true){
-                cardShower[i].childNodes[1].textContent = "Ver menos"
-            }else{
-                cardShower[i].childNodes[1].textContent = "Ver más"
-            }
             cardShower[i].previousElementSibling.classList.toggle('hidden')
+            cardShower[i].childNodes[1].classList.toggle('shower-indicator')
         })
     }
 }
@@ -24,15 +27,24 @@ const loadCatalog = (data) => {
     main.innerHTML = ''
     for (let i = 0; i < data.length; i++){        
         let cloneTemplate = cardTemplate.cloneNode(true)
-        cloneTemplate.querySelector('.card__img').setAttribute('src', "./images/" + data[i].image)
+        
+        for (let j = 0; j < data[i].image.length; j++){
+            cloneTemplate.querySelector('.card__img__container').innerHTML += `<img class="card__img" src="./images/${data[i].image[j]}" alt="VALERUKI">`
+        }
+        
         cloneTemplate.querySelector('.card__title').textContent =  data[i].title
         cloneTemplate.querySelector('.card__main-info__cat').textContent = data[i].category
         cloneTemplate.querySelector('.card__main-info__price').textContent = data[i].price
-        cloneTemplate.querySelector('.card__description').textContent = data[i].description
+        cloneTemplate.querySelector('.card__description').innerHTML = data[i].description + '<br><br>' + cuidados
+        cloneTemplate.querySelector('.wa').setAttribute('href', 'https://wa.me/573113396584?text=' + 'Enlace al producto: http://192.68.20.65:5500/' + data[i].id)
         fragment.appendChild(cloneTemplate)
     }
-    main.appendChild(fragment)
-    fragment.innerHTML = ''
+    
+    if (data.length == 0){
+        main.innerHTML = `<h3 class='info'>Sin resultados</h3>`
+    }else{
+        main.appendChild(fragment)
+    }    
     enableShower()
 }
 
@@ -43,10 +55,40 @@ const searchInCatalog = () => {
     loadCatalog(catalogFiltered)
 }
 
+const filterInCatalog = (selected) => {
+    let getSearch = selected    
+
+    if (getSearch == 'Ver todos'){
+        loadCatalog(catalog)
+    }else{
+        let expression = new RegExp(`${getSearch}.*`, 'i')
+        const catalogFiltered = catalog.filter(item => expression.test(item.category))     
+        loadCatalog(catalogFiltered)
+    }    
+}
+
+// Listeners *************************************************************
+
 window.addEventListener('DOMContentLoaded', () => {
+    catalog.reverse()
     loadCatalog(catalog)
 })
 
 searchInput.addEventListener('input', () => {
     searchInCatalog(catalog)
 })
+
+filtersBtn.addEventListener('click', () => {
+    filter.classList.toggle('hidden-outside')
+})
+
+closeFilterBtn.addEventListener('click', () => {
+    filter.classList.toggle('hidden-outside')
+})
+
+for (let i = 0; i < filterItems.length; i++){
+    filterItems[i].addEventListener('click', () => {
+        filterInCatalog(filterItems[i].textContent)
+        filter.classList.add('hidden-outside')
+    })
+}
--- data.js
+++ data.js
@@ -1,5 +1,79 @@
-catalog = [
-    {"id": "001", "title": "Desde la montaña", "category": "Aretes", "price" : "$99.000", "description": "Las piezas tejidas en nuestro taller tienen un simbolismo especial.Hemos simplificado la obra de arte: DESDE LA MONTAÑA, para las mujeres que usan aretes más cortos.Recordemos que estás piezas son inspiradas en la Serranía de Coraza municipio de Colosó-Sucre.", "image": "001.jpg"},
-    {"id": "002", "title": "Aretes simbólicos: Roble florecido", "category": "Aretes", "price" : "$99.002", "description": "Las piezas tejidas en nuestro taller tienen un simbolismo especial.Hemos simplificado la obra de arte: DESDE LA MONTAÑA, para las mujeres que usan aretes más cortos.Recordemos que estás piezas son inspiradas en la Serranía de Coraza municipio de Colosó-Sucre.", "image": "002.jpg"},    {"id": "003", "title": "Pulsera peyote", "category": "Pulsera", "price" : "$99.002", "description": "Las piezas tejidas en nuestro taller tienen un simbolismo especial.Hemos simplificado la obra de arte: DESDE LA MONTAÑA, para las mujeres que usan aretes más cortos.Recordemos que estás piezas son inspiradas en la Serranía de Coraza municipio de Colosó-Sucre.", "image": "003.jpg"}
+cuidados = `
+    <h4>Cuidados y Recomendaciones:</h4>
+    <ul class='list'>
+        <li>Evita el contacto con productos químicos o de aseo en general.</li>
+        <li>Las cremas, el perfume y el agua pueden alterar su color original.</li>
+        <li>Evita golpear tu joya.</li>
+        <li>No la uses para bañarte en piscinas o en el mar. </li>
+        <li>Guárdala por separado para evitar desgaste o enredos por roce con otros accesorios.</li>
+    </ul>
+    <br>
+    <h4 style='text-align: center;'><i><b>Realizamos envíos nacionales.</b></i></h4>
+`
+
+catalog = [   
+
+    {"id": "002", 
+    "title": "SIMBÓLICOS: LA MARQUESA", 
+    "category": "Aretes Simbólicos", 
+    "price" : "$50.000", 
+    "image": ["002-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> Inspirada en la encantadora Mompox, el portal de la Marquesa, con vista hacia el río, se caracteriza por sus ventanas e inmensos portales, techos altos y una arquitectura colonial. <br><br> <b>Materiales: </b> Mostacilla japonesa. Hilo encerado. Topos de acero dorado."},        
+
+    {"id": "003", 
+    "title": "ARETES HEXAGONALES", 
+    "category": "Aretes", 
+    "price" : "$230.000", 
+    "image": ["003-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> <b>Materiales: </b> Mostacilla japonesa. Hilo encerado. Topos de acero dorado."}, 
+
+    {"id": "004", 
+    "title": "PULSERA", 
+    "category": "Pulsera Mujer", 
+    "price" : "$20.000", 
+    "image": ["004-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> Sistema de cierre con nudo corredizo para graduar a tu muñeca. <b>Materiales: </b> Mostacilla japonesa. Hilo chino."}, 
+
+    {"id": "005", 
+    "title": "PULSERA PEYOTE", 
+    "category": "Pulsera Mujer", 
+    "price" : "$35.000", 
+    "image": ["005-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> Sistema de cierre con nudo corredizo para graduar a tu muñeca. <br><br> <b>Materiales: </b> Mostacilla japonesa. Hilo encerado"}, 
+
+    {"id": "006", 
+    "title": "JOYAS CON PIEZAS DE ORO 18 QUILATES", 
+    "category": "Set de pulseras", 
+    "price" : "$135.000", 
+    "image": ["006-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> Sistema de cierre con nudo corredizo para graduar a tu muñeca. <br><br> <b>Materiales: </b> Mostacilla japonesa. Hilos con brillo. Hilo encerado. 3 balines de oro 18 quilates N°4mm."}, 
+
+    {"id": "007", 
+    "title": "COLLAR CHOKER", 
+    "category": "Collar", 
+    "price" : "$25.000", 
+    "image": ["007-01.jpg","007-02.jpg","007-03.jpg","007-04.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> <b>Materiales: </b> Mostacilla japonesa. Sistema de cierre con herrajes de acero dorado. Medida aproximada de choker entre 36 a 37 cm con extensión de cadena."}, 
+
+    {"id": "008", 
+    "title": "ARETES HEXAGONALES", 
+    "category": "Aretes", 
+    "price" : "$80.000", 
+    "image": ["008-01.jpg"],
+    "description": "Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre. <br><br> <b>Materiales: </b> Mostacilla japonesa. Hilo encerado. Topo en acero dorado."}, 
+
+
 ]
 
+
+
+/*
+
+    {"id": "", 
+    "title": "", 
+    "category": "", 
+    "price" : "$", 
+    "image": [".jpg"],
+    "description": ""}, 
+
+*/
