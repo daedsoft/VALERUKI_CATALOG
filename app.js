9f3b63e2203e71b903a3eee2ca6204af8ca18acf
DIEGO ESCOBAR <you@example.com>
2023-04-28 07:14:35 -0500

    ajustes de codigo y nueva data

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ const autoComplete = document.querySelector('.autocomplete')
 const filtersBtn = document.querySelector('#filtersBtn')
 const filter = document.querySelector('.filter')
 const closeFilterBtn = document.querySelector('#closeFilterBtn')
+const filterBody = document.querySelector('.filter__body')
+
+// load filter options ********************************************
+
+categories.map((i) => {    
+    filterBody.innerHTML += `<p class="filter__item" data-id=${i.id}>${i.name}</p>`        
+})
 const filterItems = document.querySelectorAll('.filter__item')
 
 // Functions ****************************************************
-
 
 const enableShower = () => {
     const cardShower = document.querySelectorAll('.card__shower')
@@ -33,7 +39,7 @@ const loadCatalog = (data) => {
         }
         
         cloneTemplate.querySelector('.card__title').textContent =  data[i].title
-        cloneTemplate.querySelector('.card__main-info__cat').textContent = data[i].category
+        cloneTemplate.querySelector('.card__main-info__cat').textContent = categories.filter(c => c.id == data[i].category)[0].name 
         cloneTemplate.querySelector('.card__main-info__price').textContent = data[i].price
         cloneTemplate.querySelector('.card__description').innerHTML = intro + data[i].description + '<br><br>' + cuidados
         cloneTemplate.querySelector('.wa').setAttribute('href', 'https://wa.me/573113396584?text=Estoy interesada en este accesorio: ' + 'https://daedsoft.github.io/VALERUKI_CATALOG/item.html?id=' + data[i].id)
@@ -56,13 +62,10 @@ const searchInCatalog = () => {
 }
 
 const filterInCatalog = (selected) => {
-    let getSearch = selected    
-
-    if (getSearch == 'Ver todos'){
+    if (selected == '0'){
         loadCatalog(catalog)
-    }else{
-        let expression = new RegExp(`${getSearch}.*`, 'i')
-        const catalogFiltered = catalog.filter(item => expression.test(item.category))     
+    }else{        
+        const catalogFiltered = catalog.filter(item => item.category == selected)     
         loadCatalog(catalogFiltered)
     }    
 }
@@ -88,7 +91,7 @@ closeFilterBtn.addEventListener('click', () => {
 
 for (let i = 0; i < filterItems.length; i++){
     filterItems[i].addEventListener('click', () => {
-        filterInCatalog(filterItems[i].textContent)
+        filterInCatalog(filterItems[i].getAttribute("data-id"))
         filter.classList.add('hidden-outside')
     })
 }
